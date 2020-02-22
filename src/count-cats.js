95d8acde40c417aa80b7325719966464c62644d4
DenyingTheTruth <anton.bachikin@gmail.com>
2020-02-22 15:34:04 +0300

    feat: implement functions "carbon-dating" & "count-cats"

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return matrix && matrix.length
+    ? matrix.reduce((a, b) => [...a, ...b]).filter(el => el == "^^").length
+    : 0;
 };
